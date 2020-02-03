b5d8231ba3035964ab9c3003f2d414199de8f441
David <mwikyakimolo@gmail.com>
2020-02-03 03:51:53 +0300

    created a window

---
+++ src/main.py
@@ -0,0 +1,18 @@
+# imports
+import tkinter
+
+
+# initialize tkinter
+main_window = tkinter.Tk()
+
+# main window title
+main_window.title("Favorite Crypto Price Checker")
+
+# make the screen size
+main_window.geometry("500x800")
+
+
+
+
+# main window event loop
+main_window.mainloop()
