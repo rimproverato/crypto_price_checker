5547c713f8a4694717f1e792924e509f372ae879
David <mwikyakimolo@gmail.com>
2020-02-03 20:59:50 +0300

    api calls to get market cap and price

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 # imports
 import tkinter
+import requests
 
+coin_marketcap_api = "https://api.coinmarketcap.com/v1/ticker/"
 # calculate market cap
+def check_marketcap(coin_name):
+    web_response  = requests.get(coin_marketcap_api + coin_name) 
+    web_response_json = web_response.json()
+    return(web_response_json[0]["market_cap_usd"])
 
 # calculate price 
+def check_price(coin_name):
+    web_response  = requests.get(coin_marketcap_api + coin_name) 
+    web_response_json = web_response.json()
+    return(web_response_json[0]["price_usd"])
 
 # initialize tkinter
 main_window = tkinter.Tk()
@@ -40,11 +50,11 @@ bitcoin = tkinter.Label(text = " Bitcoin", font = ("times", 15))
 bitcoin.grid(row = 1, column = 0)
 
 # bitcoin marketcap
-bitcoin_marketcap = tkinter.Button(text = " Marketcap", font = ("times", 15))
+bitcoin_marketcap = tkinter.Button(text = check_marketcap("bitcoin"), font = ("times", 15))
 bitcoin_marketcap.grid(row = 1, column = 1)
 
 # bitcoin price
-bitcoin_price = tkinter.Button(text = " Price", font = ("times", 15))
+bitcoin_price = tkinter.Button(text = f"USD {check_price('bitcoin')}", font = ("times", 15))
 bitcoin_price.grid(row = 1, column = 2)
 
 # ethereum
@@ -52,23 +62,23 @@ bitcoin = tkinter.Label(text = "  Ethereum", font = ("times", 15))
 bitcoin.grid(row = 2, column = 0)
 
 # ethereum marketcap
-bitcoin_marketcap = tkinter.Button(text = " Marketcap", font = ("times", 15))
-bitcoin_marketcap.grid(row = 2, column = 1)
+ethereum_marketcap = tkinter.Button(text = check_marketcap("ethereum"), font = ("times", 15))
+ethereum_marketcap.grid(row = 2, column = 1)
 
 # ethereum price
-ethereum_price = tkinter.Button(text = " Price", font = ("times", 15))
+ethereum_price = tkinter.Button(text = f"USD {check_price('ethereum')}", font = ("times", 15))
 ethereum_price.grid(row = 2, column = 2)
 
 # monero
 bitcoin = tkinter.Label(text = "Monero", font = ("times", 15))
 bitcoin.grid(row = 3, column = 0)
 
-# ethereum marketcap
-monero_marketcap = tkinter.Button(text = " Marketcap", font = ("times", 15))
+# monero marketcap
+monero_marketcap = tkinter.Button(text = f"USD {check_marketcap('monero')}", font = ("times", 15))
 monero_marketcap.grid(row = 3, column = 1)
 
-# ethereum price
-monero_price = tkinter.Button(text = " Price", font = ("times", 15))
+# monero price
+monero_price = tkinter.Button(text = check_price("monero"), font = ("times", 15))
 monero_price.grid(row = 3, column = 2)
 
 # main window event loop
